593b464703d05a861c60f31c93c625917bfa4273
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2025-09-16 23:04:17 +0530

    Updated AppApplication.java

---
+++ src/main/java/com.example.employee/AppApplication.java
@@ -0,0 +1,9 @@
+package com.example.employee;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AppApplication extends Application {
+    // JAX-RS application root
+}
